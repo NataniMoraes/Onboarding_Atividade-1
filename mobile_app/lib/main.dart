28966642b3570d7bc5ec3b7ae67d4193d2580d39
Natani Moraes <natanigabriele@gmail.com>
2025-12-02 17:15:27 -0300

    Update main.dart

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -76,15 +76,19 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+
+//Alteração do título do AppBar
       appBar: AppBar(
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
+
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Mobile OK - Flutter'),
       ),
+
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -104,10 +108,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+//Modificação do contador
+            const Text('Ambiente Flutter Configurado!',
+              style: TextStyle(fontSize: 18),
             ),
           ],
         ),
